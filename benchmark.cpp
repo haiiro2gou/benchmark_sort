1f02fdf6d6c2dee623fa32d6b115e1429edc0b2c
haiiro2gou <mrashen.2gou@gmail.com>
2023-11-19 02:10:07 +0900

    まとめておく

--- benchmark.cpp
+++ benchmark.cpp
@@ -77,74 +77,78 @@ int main() {
 
     // input phase
     int n, size; string type;
-    bool ipcheck = false;
-    while (!ipcheck) {
-        int T = input("Type", "Determines the type of sorting that will take place during benchmarking (1: Number, 2: Size)", 1, 2);
-        type = (T == 1 ? "Number" : "Size");
-        int N = input("N", "Determines the number of elements to be sorted", 0, 7);
-        n = pow(10, N);
-        if (T == 2) {
-            int S = input("Size", "Specifies the size of the data to be sorted", 1, 7);
-            size = pow(10, S);
-        }
-        else size = 1000;
-        
-        fprintf(stderr, "Your input was...\n");
-        fprintf(stderr, "Type: %s\n", type.c_str());
-        fprintf(stderr, "Elements: %ld\n", n);
-        if (T == 2) fprintf(stderr, "Size: %ld\n", size);
+    {
+        bool ipcheck = false;
+        while (!ipcheck) {
+            int T = input("Type", "Determines the type of sorting that will take place during benchmarking (1: Number, 2: Size)", 1, 2);
+            type = (T == 1 ? "Number" : "Size");
+            int N = input("N", "Determines the number of elements to be sorted", 0, 7);
+            n = pow(10, N);
+            if (T == 2) {
+                int S = input("Size", "Specifies the size of the data to be sorted", 1, 7);
+                size = pow(10, S);
+            }
+            else size = 1000;
+            
+            fprintf(stderr, "Your input was...\n");
+            fprintf(stderr, "Type: %s\n", type.c_str());
+            fprintf(stderr, "Elements: %ld\n", n);
+            if (T == 2) fprintf(stderr, "Size: %ld\n", size);
 
-        ipcheck = input("Check", "Is this input correct? (Yes: 1 / No: 0)", 0, 1);
+            ipcheck = input("Check", "Is this input correct? (Yes: 1 / No: 0)", 0, 1);
 
-        if (!ipcheck) {
-            fprintf(stderr, "\r\033[1A\r\033[0K\r\033[1A\r\033[0K\r\033[1A\r\033[0K");
-            if (T == 2) fprintf(stderr, "\r\033[1A\r\033[0K");
+            if (!ipcheck) {
+                fprintf(stderr, "\r\033[1A\r\033[0K\r\033[1A\r\033[0K\r\033[1A\r\033[0K");
+                if (T == 2) fprintf(stderr, "\r\033[1A\r\033[0K");
+            }
+            else fprintf(stderr, "\n");
         }
-        else fprintf(stderr, "\n");
     }
 
     chrono::system_clock::time_point proc_start = chrono::system_clock::now();
 
     // data creation phase
-    fprintf(stderr, "Data generation has started...\n");
-    
-    chrono::system_clock::time_point gen_start = chrono::system_clock::now();
     vector<val_t> list(n);
-    string abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    prog_bar gen_1 = {0, n};
-    bar_create(gen_1);
-    chrono::system_clock::time_point gen_1_cp = gen_start;
-    for (int i = 0; i < n; i++) {
-        gen_1.current++;
-        chrono::system_clock::time_point gen_1_now = chrono::system_clock::now();
-        if (static_cast<int>(chrono::duration_cast<chrono::milliseconds>(gen_1_now - gen_1_cp).count() / 200.0)) {
-            bar_update(gen_1);
-            gen_1_cp = gen_1_now;
-        }
-        prog_bar gen_2 = {0, size};
-        bar_create(gen_2);
-        chrono::system_clock::time_point gen_2_cp = gen_1_now;
-
-        random_device seed_gen;
-        mt19937 engine(seed_gen());
-        list[i].key = engine() % inf;
-        list[i].data = "";
-        for (int j = 0; j < size; j++) {
-            gen_2.current++;
-            chrono::system_clock::time_point gen_2_now = chrono::system_clock::now();
-            if (static_cast<int>(chrono::duration_cast<chrono::milliseconds>(gen_2_now - gen_2_cp).count() / 200.0)) {
-                bar_update(gen_2);
-                gen_2_cp = gen_2_now;
+    {
+        fprintf(stderr, "Data generation has started...\n");
+        
+        chrono::system_clock::time_point gen_start = chrono::system_clock::now();
+        string abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        prog_bar gen_1 = {0, n};
+        bar_create(gen_1);
+        chrono::system_clock::time_point gen_1_cp = gen_start;
+        for (int i = 0; i < n; i++) {
+            gen_1.current++;
+            chrono::system_clock::time_point gen_1_now = chrono::system_clock::now();
+            if (static_cast<int>(chrono::duration_cast<chrono::milliseconds>(gen_1_now - gen_1_cp).count() / 200.0)) {
+                bar_update(gen_1);
+                gen_1_cp = gen_1_now;
             }
+            prog_bar gen_2 = {0, size};
+            bar_create(gen_2);
+            chrono::system_clock::time_point gen_2_cp = gen_1_now;
 
-            list[i].data += abc[engine() % 52];
+            random_device seed_gen;
+            mt19937 engine(seed_gen());
+            list[i].key = engine() % inf;
+            list[i].data = "";
+            for (int j = 0; j < size; j++) {
+                gen_2.current++;
+                chrono::system_clock::time_point gen_2_now = chrono::system_clock::now();
+                if (static_cast<int>(chrono::duration_cast<chrono::milliseconds>(gen_2_now - gen_2_cp).count() / 200.0)) {
+                    bar_update(gen_2);
+                    gen_2_cp = gen_2_now;
+                }
+
+                list[i].data += abc[engine() % 52];
+            }
+            bar_delete();
         }
         bar_delete();
-    }
-    bar_delete();
-    chrono::system_clock::time_point gen_end = chrono::system_clock::now();
+        chrono::system_clock::time_point gen_end = chrono::system_clock::now();
 
-    fprintf(stderr, "Data generation has completed! (Elapsed: %s)\n", time_elapsed(gen_start, gen_end).c_str());
+        fprintf(stderr, "Data generation has completed! (Elapsed: %s)\n", time_elapsed(gen_start, gen_end).c_str());
+    }
 
     // main phase
 
