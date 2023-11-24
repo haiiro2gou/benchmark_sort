52dc650e4f5a67260640738059b33d3537e7d16f
はいいろ <mrashen.2gou@gmail.com>
2023-11-24 16:44:38 +0900

    進捗表示を更新

--- benchmark.cpp
+++ benchmark.cpp
@@ -28,7 +28,7 @@ int input(std::string name, std::string desc, int min, int max) {
 
     int res;
     bool check = false;
-    fprintf(stderr, "Please input '%s' (%d ~ %d)\n(%s)\n", name.c_str(), min, max, desc.c_str());
+    fprintf(stderr, "\r\033[0KPlease input '%s' (%d ~ %d)\n\r\033[0K(%s)\n", name.c_str(), min, max, desc.c_str());
     while (!check) {
         fprintf(stderr, "\r\033[0K%s> ", name.c_str());
         std::cin >> res;
@@ -69,29 +69,35 @@ std::string time_elapsed(std::chrono::system_clock::time_point start, std::chron
 }
 
 std::chrono::system_clock::time_point latest;
-void bar_create(prog_bar conf) {
-    fprintf(stderr, "%s 0 / %d (0ms)\n[..............................] 0.0 %%\n", conf.title.c_str(), conf.max);
-    latest = std::chrono::system_clock::now();
+vector<prog_bar> bar;
+void bar_push(prog_bar conf) {
+    bar.push_back(conf);
+    bar_update(true);
 }
 
-void bar_update(prog_bar conf, bool force = false) {
+void bar_update(bool force = false) {
     std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
     if (static_cast<int>(std::chrono::duration_cast<std::chrono::milliseconds>(now - latest).count()) > 200 || force) {
-        latest = std::chrono::system_clock::now();
-
-        double perc = 100.0 * conf.current / conf.max;
-        std::string bar = "";
-        for (int i = 0; i < 30; i++) {
-            if (conf.current * 30 >= conf.max * (i + 1)) bar += '#';
-            else bar += '.';
+        for (auto &t : bar) {
+            double percent = 100.0 * t.current / t.max;
+            std::string progress = "";
+            for (int i = 0; i < 30; i++) {
+                if (t.current * 30 >= t.max / (i + 1)) progress += "#";
+                else progress += ".";
+            }
+            
+            fprintf(stderr, "\r\033[0K%s %d / %d (%s)\n", t.title.c_str(), t.current, t.max, time_elapsed(t.start, now).c_str());
+            fprintf(stderr, "\r\033[0K[%s] %.1lf %%\n", progress.c_str(), percent);
         }
-
-        fprintf(stderr, "\r\033[1A\r\033[0K\r\033[1A\r\033[0K%s %d / %d (%s)\n[%s] %.1lf %%\n", conf.title.c_str(), conf.current, conf.max, time_elapsed(conf.start, now).c_str(), bar.c_str(), perc);
+        for (int i = 0; i < bar.size(); i++) { fprintf(stderr, "\r\033[2A"); }
+        
+        latest = now;
     }
 }
 
-void bar_delete() {
-    fprintf(stderr, "\r\033[1A\r\033[0K\r\033[1A\r\033[0K");
+void bar_pop() {
+    bar.pop_back();
+    bar_update(true);
 }
 
 std::vector<int> get_candidate(int start, int end) {
@@ -108,9 +114,9 @@ const int inf = INT_MAX / 2;
 int main() {
     using namespace std;
 
-    fprintf(stderr, "Sort Bench v0.2.6\n");
-    fprintf(stderr, "Made by @haiiro2gou\n");
-    fprintf(stderr, "\n");
+    fprintf(stderr, "\r\033[0KSort Bench v0.2.6\n");
+    fprintf(stderr, "\r\033[0KMade by @haiiro2gou\n");
+    fprintf(stderr, "\r\033[0K\n");
 
     // input phase
     int n, size, a; string type; bool skip = false;
@@ -132,12 +138,12 @@ int main() {
             int A = input("Multiplier", "Determines the number of attempts.", 0, 4);
             a = pow(10, A);
             
-            fprintf(stderr, "Your input was...\n");
-            fprintf(stderr, "Type: %s\n", type.c_str());
-            fprintf(stderr, "Elements: %d\n", n);
-            fprintf(stderr, "Size: %d\n", size);
-            fprintf(stderr, "Attempts: %d\n", a);
-            fprintf(stderr, "\n");
+            fprintf(stderr, "\r\033[0KYour input was...\n");
+            fprintf(stderr, "\r\033[0KType: %s\n", type.c_str());
+            fprintf(stderr, "\r\033[0KElements: %d\n", n);
+            fprintf(stderr, "\r\033[0KSize: %d\n", size);
+            fprintf(stderr, "\r\033[0KAttempts: %d\n", a);
+            fprintf(stderr, "\r\033[0K\n");
 
             ipcheck = input("Check", "Is your input correct? (Yes: 1 / No: 0)", -1, 1);
             
@@ -154,13 +160,13 @@ int main() {
     chrono::system_clock::time_point all_start = chrono::system_clock::now();
 
     if (skip) {
-        fprintf(stderr, "Skip has been selected!\n");
+        fprintf(stderr, "\r\033[0KSkip has been selected!\n");
     }
     else {
         // main phase
         vector<vector<vector<double>>> result(target.size(), vector<vector<double>>(a, vector<double>(10)));
         {
-            fprintf(stderr, "Main Process has started...\n");
+            fprintf(stderr, "\r\033[0KMain Process has started...\n");
 
             const std::string abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
             random_device seed_gen;
@@ -170,25 +176,25 @@ int main() {
             vector<val_t> list(n);
 
             prog_bar main = {"Main Loop", 0, (type == "element" ? n : size), chrono::system_clock::now()};
-            bar_create(main);
+            bar_push(main);
             for (auto &t : target) {
-                main.current = t;
-                bar_update(main, true);
+                bar.back.current = t;
+                bar_update();
 
                 if (type == "element") list.resize(t);
 
                 prog_bar l1 = {"Attempts", 0, a, chrono::system_clock::now()};
-                bar_create(l1);
+                bar_push(l1);
                 for (int i = 0; i < a; i++) {
-                    l1.current++;
-                    bar_update(l1, true);
+                    bar.back.current++;
+                    bar_update();
 
                     // main sort phase
                     prog_bar gen = {"Randomizer", 0, (type == "element" ? t : n), chrono::system_clock::now()};
-                    bar_create(gen);
+                    bar_push(gen);
                     for (int g1 = 0; g1 < (type == "element" ? t : n); g1++) {
-                        gen.current++;
-                        bar_update(gen);
+                        bar.back.current++;
+                        bar_update();
 
                         mt19937 engine(seed_gen());
                         list[i].key = engine() % inf;
@@ -197,15 +203,15 @@ int main() {
                             list[i].data += abc[engine() % 52];
                         }
                     }
-                    bar_delete();
+                    bar_pop();
 
                     result[marker][i] = sort_launch(list);
                 }
-                bar_delete();
+                bar_pop();
 
                 marker++;
             }
-            bar_delete();
+            bar_pop();
             chrono::system_clock::time_point main_end = chrono::system_clock::now();
 
             fprintf(stderr, "\r\033[1A\r\033[0KMain Process has completed! (Elapsed: %s)\n", time_elapsed(main_start, main_end).c_str());
@@ -213,8 +219,8 @@ int main() {
 
         // output phase
         {
-            fprintf(stderr, "Benchmark results are now being exported.\n");
-            fprintf(stderr, "Caution: If the file exists in the output area, delete the files\n");
+            fprintf(stderr, "\r\033[0KBenchmark results are now being exported.\n");
+            fprintf(stderr, "\r\033[0KCaution: If the file exists in the output area, delete the files\n");
 
             chrono::system_clock::time_point output_start = chrono::system_clock::now();
             
@@ -233,7 +239,7 @@ int main() {
 
     // zip phase
     {
-        fprintf(stderr, "Output data is now being compressed.\n");
+        fprintf(stderr, "\r\033[0KOutput data is now being compressed.\n");
 
         chrono::system_clock::time_point zip_start = chrono::system_clock::now();
 
@@ -260,7 +266,7 @@ int main() {
     }
 
     chrono::system_clock::time_point all_end = chrono::system_clock::now();
-    fprintf(stderr, "All process has been completed! (Elapsed: %s)\n", time_elapsed(all_start, all_end).c_str());
+    fprintf(stderr, "\r\033[0KAll process has been completed! (Elapsed: %s)\n", time_elapsed(all_start, all_end).c_str());
 
     return 0;
 }
