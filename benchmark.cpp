ba988d521195ace61c69f2d153c6487738320794
haiiro2gou <mrashen.2gou@gmail.com>
2023-11-24 18:41:48 +0900

    v0.2.9

--- benchmark.cpp
+++ benchmark.cpp
@@ -119,7 +119,7 @@ const int inf = INT_MAX / 2;
 int main() {
     using namespace std;
 
-    fprintf(stderr, "\r\033[0KSort Bench v0.2.8\n");
+    fprintf(stderr, "\r\033[0KSort Bench v0.2.9\n");
     fprintf(stderr, "\r\033[0KMade by @haiiro2gou\n");
     fprintf(stderr, "\r\033[0K\n");
 
