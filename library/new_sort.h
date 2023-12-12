70b0dedf960e3a0b14618c69a27e6855cb56ad72
haiiro2gou <mrashen.2gou@gmail.com>
2023-12-13 00:50:47 +0900

    new sort rework

--- library/new_sort.h
+++ library/new_sort.h
@@ -49,7 +49,7 @@ void new_sort(RAI a0, RAI aN, C cmp)
     const double n_dig = log10(N);
     const double s_dig = log10((*a0).data.length());
 
-    if (n_dig - s_dig > -1) {
+    if (n_dig - s_dig > 1) {
         imake_heap(a0, N, cmp);
         isort_heap(a0, N, cmp);
     }
